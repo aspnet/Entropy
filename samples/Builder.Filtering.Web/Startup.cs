3f8e2595158f6a8eff0751649a9d69faa1b507a1
Louis DeJardin <lodejard@microsoft.com>
2014-02-24 13:35:58 -0800

    Adding projects for IBuilder usage patterns

---
+++ samples/Builder.Filtering.Web/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Builder.Filtering.Web
+{
+    public class Startup
+    {
+        public void Configuration(IBuilder app)
+        {
+            app.Use(next => context => FilterAsync(context, next));
+
+            app.Run(HelloWorldAsync);
+        }
+
+        public async Task FilterAsync(HttpContext context, RequestDelegate next)
+        {
+            var body = context.Response.Body;
+            var buffer = new MemoryStream();
+            context.Response.Body = buffer;
+
+            try
+            {
+                context.Response.Headers["CustomHeader"] = "My Header";
+
+                await context.Response.WriteAsync("Before\r\n");
+                await next(context);
+                await context.Response.WriteAsync("After\r\n");
+
+                buffer.Position = 0;
+                await buffer.CopyToAsync(body);
+            }
+            finally
+            {
+                context.Response.Body = body;
+            }
+        }
+
+        public async Task HelloWorldAsync(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            await context.Response.WriteAsync("Hello world\r\n");
+        }
+    }
+}
---
+++ samples/Builder.Middleware.Web/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.Abstractions;
+using Microsoft.AspNet.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Middleware.Web
+{
+    public class Startup
+    {
+        public void Configuration(IBuilder app)
+        {
+            app.Use(typeof(MyMiddleware), "Yo");
+        }
+    }
+
+    public static class BuilderExtensions
+    {
+        public static IBuilder Use(this IBuilder builder, Type middleware, params object[] args)
+        {
+            return builder.Use(next =>
+            {
+                var typeActivator = builder.ServiceProvider.GetService<ITypeActivator>();
+                var instance = typeActivator.CreateInstance(middleware, new[] { next }.Concat(args).ToArray());
+                return (RequestDelegate)Delegate.CreateDelegate(typeof(RequestDelegate), instance, "Invoke");
+            });
+        }
+    }
+
+    public class MyMiddleware
+    {
+        private RequestDelegate _next;
+        private string _greeting;
+        private IServiceProvider _services;
+
+        public MyMiddleware(RequestDelegate next, string greeting, IServiceProvider services)
+        {
+            _next = next;
+            _greeting = greeting;
+            _services = services;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync(_greeting + ", middleware!");
+        }
+    }
+}
