0d8f01121b60a32b2bf7fda1751d1f4711f6ed33
Louis DeJardin <lodejard@microsoft.com>
2014-03-11 02:01:13 -0700

    Writing a custom config source

---
+++ samples/Config.CustomSource.Web/MyConfigSource.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.ConfigurationModel.Sources;
+
+namespace Config.CustomSource.Web
+{
+    public class MyConfigSource : IConfigurationSource
+    {
+        public bool TryGet(string key, out string value)
+        {
+            switch (key)
+            {
+                case "Hardcoded:1:Caption":
+                    value = "One";
+                    return true;
+                case "Hardcoded:2:Caption":
+                    value = "Two";
+                    return true;
+            }
+            value = null;
+            return false;
+        }
+
+        public void Load()
+        {
+            // no loading or reloading, this source is all hardcoded
+        }
+        
+        public IEnumerable<string> ProduceSubKeys(IEnumerable<string> earlierKeys, string prefix, string delimiter)
+        {
+            // TODO: ProduceSubKeys method signature is pretty bad
+
+            if (prefix == "" && delimiter == ":")
+            {
+                return earlierKeys.Concat(new[] { "Hardcoded" });
+            }
+            if (prefix == "Hardcoded:" && delimiter == ":")
+            {
+                return earlierKeys.Concat(new[] { "1", "2" });
+            }
+            if (prefix == "Hardcoded:1:" || prefix == "Hardcoded:2:")
+            {
+                return earlierKeys.Concat(new[] { "Caption" });
+            }
+            return earlierKeys;
+        }
+    }
+}
---
+++ samples/Config.CustomSource.Web/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Config.CustomSource.Web;
+using Microsoft.AspNet.Abstractions;
+using Microsoft.AspNet.ConfigurationModel;
+
+public class Startup
+{
+    public void Configuration(IBuilder app)
+    {
+        var config = new Configuration
+        {
+            new MyConfigSource()
+        };
+
+        app.Run(async ctx =>
+        {
+            ctx.Response.ContentType = "text/plain";
+            await DumpConfig(ctx.Response, config);
+        });
+    }
+
+    private static async Task DumpConfig(HttpResponse response, IConfiguration config, string indentation = "")
+    {
+        foreach (var child in config.GetSubKeys())
+        {
+            await response.WriteAsync(indentation + "[" + child.Key + "] " + config.Get(child.Key) + "\r\n");
+            await DumpConfig(response, child.Value, indentation + "  ");
+        }
+    }
+}
