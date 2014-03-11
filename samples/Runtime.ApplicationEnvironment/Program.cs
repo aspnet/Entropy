a55ca84b709145895ba62193dba1dafe6e650175
David Fowler <davidfowl@gmail.com>
2014-03-11 09:33:30 -0700

    Added sample showing injecting application environment

---
+++ samples/Runtime.ApplicationEnvironment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Net.Runtime;
+
+namespace Runtime.ApplicationEnvironment
+{
+    public class Program
+    {
+        private readonly IApplicationEnvironment _environment;
+
+        public Program(IApplicationEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public void Main()
+        {
+            Console.WriteLine("======================================================");
+            Console.WriteLine("ApplicationName: {0}", _environment.ApplicationName);
+            Console.WriteLine("ApplicationBasePath: {0}", _environment.ApplicationBasePath);
+            Console.WriteLine("TargetFramework: {0}", _environment.TargetFramework);
+            Console.WriteLine("Version: {0}", _environment.Version);
+            Console.WriteLine("======================================================");
+            Console.ReadLine();
+        }
+    }
+}
