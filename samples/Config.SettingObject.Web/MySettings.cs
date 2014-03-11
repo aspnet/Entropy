4f1b4247e970635237757d84600d4120c411485d
Louis DeJardin <lodejard@microsoft.com>
2014-03-10 22:32:08 -0700

    Settings object reading configuration

---
+++ samples/Config.SettingObject.Web/MySettings.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.ConfigurationModel;
+
+namespace Config.SettingObject.Web
+{
+    public class MySettings
+    {
+        public MySettings()
+        {
+            RetryCount = 3;
+            AdBlocks = new Dictionary<string, AdBlock>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public int RetryCount { get; set; }
+        public string DefaultAdBlock { get; set; }
+        public IDictionary<string, AdBlock> AdBlocks { get; private set; }
+
+        public void Read(IConfiguration configuration)
+        {
+            string value;
+            if (configuration.TryGet("RetryCount", out value))
+            {
+                RetryCount = int.Parse(value);
+            }
+            if (configuration.TryGet("DefaultAdBlock", out value))
+            {
+                DefaultAdBlock = value;
+            }
+
+            var items = new List<AdBlock>();
+            foreach (var subConfig in configuration.GetSubKeys("AdBlock"))
+            {
+                var item = new AdBlock { Name = subConfig.Key };
+                if (subConfig.Value.TryGet("Origin", out value))
+                {
+                    item.Origin = value;
+                }
+                if (subConfig.Value.TryGet("ProductCode", out value))
+                {
+                    item.ProductCode = value;
+                }
+                items.Add(item);
+            }
+            AdBlocks = items.ToDictionary(
+                item => item.Name, 
+                item => item,
+                StringComparer.OrdinalIgnoreCase);
+        }
+
+        public class AdBlock
+        {
+            public string Name { get; set; }
+            public string ProductCode { get; set; }
+            public string Origin { get; set; }
+        }
+    }
+}
---
+++ samples/Config.SettingObject.Web/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Config.SettingObject.Web;
+using Microsoft.AspNet.Abstractions;
+using Microsoft.AspNet.ConfigurationModel;
+using Microsoft.AspNet.ConfigurationModel.Sources;
+
+public class Startup
+{
+    public void Configuration(IBuilder app)
+    {
+        var config = new Configuration
+        {
+            new MemoryConfigurationSource
+            {
+                {"MySettings:RetryCount", "42"},
+                {"MySettings:DefaultAdBlock", "House"},
+                {"MySettings:AdBlock:House:ProductCode", "123"},
+                {"MySettings:AdBlock:House:Origin", "blob-456"},
+                {"MySettings:AdBlock:Contoso:ProductCode", "contoso2014"},
+                {"MySettings:AdBlock:Contoso:Origin", "sql-789"},
+            }
+        };
+
+        var mySettings = new MySettings();
+        mySettings.Read(config.GetSubKey("MySettings"));
+
+        app.Run(async ctx =>
+        {
+            ctx.Response.ContentType = "text/plain";
+
+            await ctx.Response.WriteAsync(string.Format("Retry Count {0}\r\n", mySettings.RetryCount));
+            await ctx.Response.WriteAsync(string.Format("Default Ad Block {0}\r\n", mySettings.DefaultAdBlock));
+            foreach (var adBlock in mySettings.AdBlocks.Values)
+            {
+                await ctx.Response.WriteAsync(string.Format(
+                    "Ad Block {0} Origin {1} Product Code {2}\r\n", 
+                    adBlock.Name, adBlock.Origin, adBlock.ProductCode));                
+            }
+        });
+    }
+}
