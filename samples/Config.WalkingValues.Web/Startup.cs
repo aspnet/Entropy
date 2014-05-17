115462398391f9d6a383fa8071685989fa18efc4
David Fowler <davidfowl@gmail.com>
2014-05-16 23:37:26 -0700

    Clean up the config samples

--- samples/Config.WalkingValues.Web/Startup.cs
+++ samples/Config.WalkingValues.Web/Startup.cs
@@ -3,16 +3,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.ConfigurationModel;
-using Microsoft.Net.Runtime;
 
 public class Startup
 {
     public void Configure(IBuilder app)
     {
-        var applicationEnvironment = (IApplicationEnvironment)app.ApplicationServices.GetService(typeof(IApplicationEnvironment));
-
         var config = new Configuration();
-        config.AddIniFile(Path.Combine(applicationEnvironment.ApplicationBasePath, "Config.Sources.ini"));
+        config.AddIniFile("Config.Sources.ini");
         config.AddEnvironmentVariables();
 
         app.Run(async ctx =>
@@ -29,31 +26,5 @@ public class Startup
             await response.WriteAsync(indentation + "[" + child.Key + "] " + config.Get(child.Key) + "\r\n");
             await DumpConfig(response, child.Value, indentation + "  ");
         }
-    }
-}
-
-
-
-namespace Microsoft.Net.Runtime
-{
-    using System;
-
-    [AssemblyNeutral]
-    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
-    public sealed class AssemblyNeutralAttribute : Attribute
-    {
-    }
-}
-
-namespace Microsoft.Net.Runtime
-{
-    using System.Runtime.Versioning;
-    [AssemblyNeutral]
-    public interface IApplicationEnvironment
-    {
-        string ApplicationName { get; }
-        string Version { get; }
-        string ApplicationBasePath { get; }
-        FrameworkName TargetFramework { get; }
     }
 }
