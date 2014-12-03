bfa338d60993001539375c611318b2507c3c9c5f
Chris Ross <Tratcher@Outlook.com>
2014-12-02 16:08:09 -0800

    Add Forms and Files upload samples.

---
+++ samples/Content.Upload.Files/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace Content.Upload.Files
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync("<html><body>");
+
+                if (string.Equals("POST", context.Request.Method, StringComparison.OrdinalIgnoreCase))
+                {
+                    var form = await context.Request.ReadFormBodyAsync();
+                    await context.Response.WriteAsync("Form received: " + form.Count() + " entries.<br>");
+
+                    foreach (var part in form)
+                    {
+                        await context.Response.WriteAsync("- Key: " + part.Key + "; Value(s): " + string.Join(", ", part.Value) + "<br>");
+                    }
+
+                    await context.Response.WriteAsync("<br>");
+
+                    var files = await context.Request.ReadFilesBodyAsync();
+                    await context.Response.WriteAsync("Files received: " + files.Count() + " entries.<br>");
+
+                    foreach (var file in files)
+                    {
+                        await context.Response.WriteAsync("- Content-Disposition: " + file.ContentDisposition + "<br>");
+                        await context.Response.WriteAsync(" - Length: " + file.Body.Length + "<br>");
+                    }
+                }
+
+                await context.Response.WriteAsync("<br>Form:");
+
+                await context.Response.WriteAsync(@"
+<FORM action = ""/"" method=""post"" enctype=""multipart/form-data"" >
+<P>
+<LABEL for=""firstname"">Description:</LABEL>
+<INPUT type=""text"" name=""description"" value=""Foo"" /><BR>
+<LABEL for=""myfile1"">File 1:</LABEL>
+<INPUT type=""file"" name=""myfile1"" /><BR>
+<LABEL for=""myfile2"">File 2:</LABEL>
+<INPUT type=""file"" name=""myfile2"" /><BR>
+<INPUT type=""submit"" value=""Send"" />
+</P>
+</FORM>");
+
+                await context.Response.WriteAsync("</body></html>");
+            });
+        }
+    }
+}
---
+++ samples/Content.Upload.Form/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace Content.Upload.Form
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync("<html><body>");
+
+                if (context.Request.HasFormContentType())
+                {
+                    var form = await context.Request.ReadFormBodyAsync();
+                    await context.Response.WriteAsync("Form received: " + form.Count() + " entries.<br>");
+
+                    foreach (var part in form)
+                    {
+                        await context.Response.WriteAsync("- Key: " + part.Key + "; Value(s): " + string.Join(", ", part.Value) + "<br>");
+                    }
+                }
+
+                await context.Response.WriteAsync("<br>Form:");
+                await context.Response.WriteAsync(@"
+<FORM action = ""/"" method = ""post"">
+<P>
+<LABEL for=""firstname"">First name:</LABEL>
+<INPUT type=""text"" name=""firstname"" value=""Foo"" /><BR>
+<LABEL for=""lastname"">Last name:</LABEL>
+<INPUT type=""text"" name=""lastname"" value=""Bar"" /><BR>
+<LABEL for=""email"">email:</LABEL>
+<INPUT type=""text"" name=""email"" value=""Foo@Bar"" /><BR>
+<INPUT type=""radio"" name=""sex"" value=""Male"" />Male<BR>
+<INPUT type=""radio"" name=""sex"" value=""Female"" />Female<BR>
+<INPUT type=""submit"" value=""Send"" /><INPUT type=""reset"" />
+</P>
+</FORM>");
+                      await context.Response.WriteAsync("</body></html>");
+            });
+        }
+    }
+}
