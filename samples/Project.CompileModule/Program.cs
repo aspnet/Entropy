aeb4d2f4956dcb60ac45dd2a9505ed90f2074443
Kai Ruhnau <kai.ruhnau@live.com>
2015-08-15 17:23:40 +0200

    Added a ICompileModule sample

---
+++ samples/Project.CompileModule/Program.cs
@@ -0,0 +1,12 @@
+namespace Project.CompileModule
+{
+    public class Program
+    {
+        public void Main()
+        {
+            var foo = new Foo();
+            foo.Bar();
+        }
+    }
+}
+
