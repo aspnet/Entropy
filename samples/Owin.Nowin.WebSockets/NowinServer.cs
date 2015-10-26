3f2532b4f4bc91c8173ac8a4381b8bfb3e41ce18
John Luo <jt.luo@mail.utoronto.ca>
2015-10-25 18:16:45 -0700

    reacting to iserverfactory refactoring

---
+++ samples/Owin.Nowin.WebSockets/NowinServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Hosting.Server;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Http.Features;
+using Microsoft.AspNet.Http.Internal;
+using Microsoft.AspNet.Owin;
+using Nowin;
+
+namespace NowinWebSockets
+{
+    public class NowinServer : IServer
+    {
+        private RequestDelegate _callback;
+        private INowinServer _nowinServer;
+        private ServerBuilder _serverBuilder;
+
+        IFeatureCollection IServer.Features { get; }
+
+        public NowinServer(ServerBuilder serverBuilder)
+        {
+            if (serverBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(serverBuilder));
+            }
+            _serverBuilder = serverBuilder;
+        }
+
+        public void Start(RequestDelegate requestDelegate)
+        {
+            _callback = requestDelegate;
+            _nowinServer = _serverBuilder.SetOwinApp(OwinWebSocketAcceptAdapter.AdaptWebSockets(HandleRequest)).Build();
+            _nowinServer.Start();
+        }
+
+        public void Dispose()
+        {
+            if (_nowinServer != null)
+            {
+                _nowinServer.Dispose();
+            }
+        }
+
+        private Task HandleRequest(IDictionary<string, object> env)
+        {
+            return _callback(new DefaultHttpContext(new OwinFeatureCollection(env)));
+        }
+    }
+}
--- samples/Owin.Nowin.WebSockets/NowinServerFactory.cs
+++ samples/Owin.Nowin.WebSockets/NowinServerFactory.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Hosting.Server;
-using Microsoft.AspNet.Http.Features;
-using Microsoft.AspNet.Owin;
 using Microsoft.Extensions.Configuration;
 using Nowin;
 
@@ -12,29 +7,9 @@ namespace NowinWebSockets
 {
     public class NowinServerFactory : IServerFactory
     {
-        private Func<IFeatureCollection, Task> _callback;
-
-        private Task HandleRequest(IDictionary<string, object> env)
+        public IServer CreateServer(IConfiguration configuration)
         {
-            return _callback(new OwinFeatureCollection(env));
-        }
-
-        public IFeatureCollection Initialize(IConfiguration configuration)
-        {
-            // TODO: Parse config
-            return new FeatureCollection();
-        }
-
-        public IDisposable Start(IFeatureCollection serverFeatures, Func<IFeatureCollection, Task> application)
-        {
-            var builder = ServerBuilder.New()
-                                       .SetAddress(IPAddress.Any)
-                                       .SetPort(5000)
-                                       .SetOwinApp(OwinWebSocketAcceptAdapter.AdaptWebSockets(HandleRequest));
-            _callback = application;
-            var server = builder.Build();
-            server.Start();
-            return server;
+            return new NowinServer(ServerBuilder.New().SetAddress(IPAddress.Any).SetPort(5000));
         }
     }
 }
