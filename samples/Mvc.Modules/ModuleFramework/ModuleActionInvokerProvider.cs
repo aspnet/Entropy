b1e0fe41d937a43376ae251d6269791bbb00ae23
Ryan Nowak <nowakra@gmail.com>
2015-11-20 17:03:59 -0800

    React to breaking changes in MVC

--- samples/Mvc.Modules/ModuleFramework/ModuleActionInvokerProvider.cs
+++ samples/Mvc.Modules/ModuleFramework/ModuleActionInvokerProvider.cs
@@ -1,15 +1,10 @@
-using System;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.Filters;
-using Microsoft.AspNet.Mvc.Formatters;
-using Microsoft.AspNet.Mvc.Infrastructure;
-using Microsoft.AspNet.Mvc.ModelBinding;
-using Microsoft.AspNet.Mvc.ModelBinding.Validation;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.OptionsModel;
 
 namespace Microsoft.AspNet.Mvc.ModuleFramework
 {
@@ -17,43 +12,13 @@ namespace Microsoft.AspNet.Mvc.ModuleFramework
     {
         private readonly IModuleFactory _moduleFactory;
         private readonly IReadOnlyList<IFilterProvider> _filterProviders;
-        private readonly IServiceProvider _serviceProvider;
-        private readonly IReadOnlyList<IInputFormatter> _inputFormatters;
-        private readonly IReadOnlyList<IOutputFormatter> _outputFormatters;
-        private readonly IReadOnlyList<IModelBinder> _modelBinders;
-        private readonly IReadOnlyList<IModelValidatorProvider> _modelValidatorProviders;
-        private readonly IReadOnlyList<IValueProviderFactory> _valueProviderFactories;
-        private readonly IActionBindingContextAccessor _actionBindingContextAccessor;
-        private readonly IOptions<MvcOptions> _optionsAccessor;
-        private readonly ILogger _logger;
-        private readonly DiagnosticSource _diagnostic;
 
         public ModuleActionInvokerProvider(
-            IModuleFactory moduleFactory,
             IEnumerable<IFilterProvider> filterProviders,
-            IReadOnlyList<IInputFormatter> inputFormatters,
-            IReadOnlyList<IOutputFormatter> outputFormatters,
-            IReadOnlyList<IModelBinder> modelBinders,
-            IReadOnlyList<IModelValidatorProvider> modelValidatorProviders,
-            IReadOnlyList<IValueProviderFactory> valueProviderFactories,
-            IActionBindingContextAccessor actionBindingContextAccessor,
-            IOptions<MvcOptions> optionsAccessor,
-            ILogger logger,
-            DiagnosticSource diagnostic,
-            IServiceProvider serviceProvider)
+            IModuleFactory moduleFactory)
         {
             _moduleFactory = moduleFactory;
             _filterProviders = filterProviders.OrderBy(p => p.Order).ToList();
-            _inputFormatters = inputFormatters;
-            _outputFormatters = outputFormatters;
-            _modelBinders = modelBinders;
-            _modelValidatorProviders = modelValidatorProviders;
-            _valueProviderFactories = valueProviderFactories;
-            _actionBindingContextAccessor = actionBindingContextAccessor;
-            _optionsAccessor = optionsAccessor;
-            _logger = logger;
-            _diagnostic = diagnostic;
-            _serviceProvider = serviceProvider;
         }
 
 
