b0673dba0f3a5d6e394498cce19cb73f3a8169ce
John Luo <jt.luo@mail.utoronto.ca>
2015-11-23 18:46:06 -0800

    reacting to new IServer IHttpApplication design

--- samples/Owin.Nowin.HelloWorld/NowinServer.cs
+++ samples/Owin.Nowin.HelloWorld/NowinServer.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Hosting.Server;
-using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Http.Features;
-using Microsoft.AspNet.Http.Internal;
 using Microsoft.AspNet.Owin;
 using Nowin;
 
@@ -12,11 +10,11 @@ namespace NowinWebSockets
 {
     public class NowinServer : IServer
     {
-        private RequestDelegate _callback;
+        private Func<IFeatureCollection, Task> _callback;
         private INowinServer _nowinServer;
         private ServerBuilder _serverBuilder;
 
-        IFeatureCollection IServer.Features { get; }
+        IFeatureCollection IServer.Features { get; } = new FeatureCollection();
 
         public NowinServer(ServerBuilder serverBuilder)
         {
@@ -27,9 +25,24 @@ namespace NowinWebSockets
             _serverBuilder = serverBuilder;
         }
 
-        public void Start(RequestDelegate requestDelegate)
+        public void Start<TContext>(IHttpApplication<TContext> application)
         {
-            _callback = requestDelegate;
+            // Note that this example does not take into account of Nowin's "server.OnSendingHeaders" callback.
+            // Ideally we should ensure this method is fired before disposing the context.
+            _callback = async features =>
+            {
+                var context = application.CreateContext(features);
+                try
+                {
+                    await application.ProcessRequestAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    application.DisposeContext(context, ex);
+                    throw;
+                }
+                application.DisposeContext(context, null);
+            };
             _nowinServer = _serverBuilder.SetOwinApp(OwinWebSocketAcceptAdapter.AdaptWebSockets(HandleRequest)).Build();
             _nowinServer.Start();
         }
@@ -44,7 +57,7 @@ namespace NowinWebSockets
 
         private Task HandleRequest(IDictionary<string, object> env)
         {
-            return _callback(new DefaultHttpContext(new OwinFeatureCollection(env)));
+            return _callback(new FeatureCollection(new OwinFeatureCollection(env)));
         }
 
     }
