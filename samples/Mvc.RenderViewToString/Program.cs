9fbc09f6bafb146b34c6a57b443a560943ba1e48
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2016-01-12 16:57:31 -0800

    Added sample to demonstrate rendering view to a string

---
+++ samples/Mvc.RenderViewToString/Program.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Hosting.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace Mvc.RenderViewToString
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Initialize the necessary services
+            var services = new ServiceCollection();
+            ConfigureDefaultServices(services, viewLocationBasePath: null);
+
+            // Add a custom service that is used in the view.
+            services.AddSingleton<EmailReportGenerator>();
+
+            var provider = services.BuildServiceProvider();
+            var helper = provider.GetRequiredService<RazorViewToStringRenderer>();
+
+            var model = new EmailViewModel
+            {
+                UserName = "User",
+                SenderName = "Sender",
+                UserData1 = 1,
+                UserData2 = 2
+            };
+            var emailContent = helper.RenderViewToString("EmailTemplate", model);
+            Console.WriteLine(emailContent);
+            Console.ReadLine();
+        }
+
+        private static void ConfigureDefaultServices(IServiceCollection services, string viewLocationBasePath)
+        {
+            var applicationEnvironment = PlatformServices.Default.Application;
+            services.AddSingleton<IHostingEnvironment>(new HostingEnvironment
+            {
+                WebRootFileProvider = new PhysicalFileProvider(viewLocationBasePath ?? applicationEnvironment.ApplicationBasePath)
+            });
+            services.AddSingleton(applicationEnvironment);
+            var diagnosticSource = new DiagnosticListener("Microsoft.AspNet");
+            services.AddSingleton<DiagnosticSource>(diagnosticSource);
+            services.AddLogging();
+            services.AddMvc();
+            services.AddSingleton<RazorViewToStringRenderer>();
+        }
+    }
+}
---
+++ samples/Mvc.RenderViewToString/RazorViewToStringRenderer.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using Microsoft.AspNet.Http.Internal;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Abstractions;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.Razor;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+using Microsoft.AspNet.Routing;
+
+namespace Mvc.RenderViewToString
+{
+    public class RazorViewToStringRenderer
+    {
+        private IRazorViewEngine _viewEngine;
+        private ITempDataProvider _tempDataProvider;
+        private IServiceProvider _serviceProvider;
+
+        public RazorViewToStringRenderer(
+            IRazorViewEngine viewEngine,
+            ITempDataProvider tempDataProvider,
+            IServiceProvider serviceProvider)
+        {
+            _viewEngine = viewEngine;
+            _tempDataProvider = tempDataProvider;
+            _serviceProvider = serviceProvider;
+        }
+
+        public string RenderViewToString<TModel>(string name, TModel model)
+        {
+            var actionContext = GetActionContext();
+
+            var viewEngineResult = _viewEngine.FindView(actionContext, name, false);
+
+            if (!viewEngineResult.Success)
+            {
+                throw new InvalidOperationException(string.Format("Couldn't find view '{0}'", name));
+            }
+
+            var view = viewEngineResult.View;
+
+            using (var output = new StringWriter())
+            {
+                var viewContext = new ViewContext(
+                    actionContext,
+                    view,
+                    new ViewDataDictionary<TModel>(
+                        metadataProvider: new EmptyModelMetadataProvider(),
+                        modelState: new ModelStateDictionary())
+                    {
+                        Model = model
+                    },
+                    new TempDataDictionary(
+                        actionContext.HttpContext,
+                        _tempDataProvider),
+                    output,
+                    new HtmlHelperOptions());
+
+                view.RenderAsync(viewContext).GetAwaiter().GetResult();
+
+                return output.ToString();
+            }
+        }
+
+        private ActionContext GetActionContext()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.RequestServices = _serviceProvider;
+            return new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+        }
+    }
+}
