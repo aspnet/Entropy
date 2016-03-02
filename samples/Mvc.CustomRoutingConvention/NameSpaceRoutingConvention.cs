20c9020c17acf46de0581ad132dfd2a2cabe7e45
Pranav K <prkrishn@hotmail.com>
2016-03-01 18:44:02 -0800

    Reacting to Mvc changes

--- samples/Mvc.CustomRoutingConvention/NameSpaceRoutingConvention.cs
+++ samples/Mvc.CustomRoutingConvention/NameSpaceRoutingConvention.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace NamespaceRouting
@@ -8,24 +9,25 @@ namespace NamespaceRouting
         {
             foreach (var controller in application.Controllers)
             {
-                if (controller.AttributeRoutes.Count == 0)
+                var hasAttributeRouteModels = controller.Selectors.Any(selector => selector.AttributeRouteModel != null);
+                if (!hasAttributeRouteModels)
                 {
-                    //Create new attribute Route for the controller
+                    // Create new attribute Route for the controller
                     var attributeRouteModel = new AttributeRouteModel();
 
-                    //Replace the . in the namespace with a / to create the attribute route 
-                    //Ex: MySite.Admin namespace will correspond to MySite/Admin attribute route
-                    //Then attach [controller], [action] and optional {id?} token. 
-                    //[Controller] and [action] is replaced with the controller and action 
-                    //name to generate the final template
+                    // Replace the . in the namespace with a / to create the attribute route
+                    // Ex: MySite.Admin namespace will correspond to MySite/Admin attribute route
+                    // Then attach [controller], [action] and optional {id?} token.
+                    // [Controller] and [action] is replaced with the controller and action
+                    // name to generate the final template
                     var template = controller.ControllerType.Namespace.Replace('.', '/') + "/[controller]/[action]/{id?}";
                     attributeRouteModel.Template = template;
 
-                    controller.AttributeRoutes.Add(attributeRouteModel);
+                    controller.Selectors.Add(new SelectorModel { AttributeRouteModel = attributeRouteModel });
                 }
             }
 
-            //You can continue to put attribute route templates for the controller actions depending on the way you want them to behave            
+            // You can continue to put attribute route templates for the controller actions depending on the way you want them to behave
         }
     }
 }
