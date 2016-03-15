ea0e50c406fe119b7523cbec57f67a2547edecdb
John Luo <jt.luo@mail.utoronto.ca>
2016-03-15 16:04:16 -0700

    Fixing tests

--- test/FunctionalTests/MvcTests/CustomRoutingConventionTests.cs
+++ test/FunctionalTests/MvcTests/CustomRoutingConventionTests.cs
@@ -2,13 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Net;
-using System.Reflection;
-using System.Runtime.Versioning;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.PlatformAbstractions;
 using Xunit;
 
 namespace BuilderTests.MvcTests
@@ -19,10 +15,8 @@ namespace BuilderTests.MvcTests
         public async Task CustomRouting_NameSpaceRouting()
         {
             // Arrange
-            var applicationEnvironment = new TestApplicationEnvironment();
             var builder = new WebHostBuilder()
-                .UseStartup(typeof(NamespaceRouting.Startup))
-                .ConfigureServices(services => services.AddSingleton<IApplicationEnvironment, TestApplicationEnvironment>());
+                .UseStartup(typeof(NamespaceRouting.Startup));
             var client = new TestServer(builder).CreateClient();
 
             // Act
@@ -32,19 +26,6 @@ namespace BuilderTests.MvcTests
             Assert.Equal(HttpStatusCode.OK, result.StatusCode);
             var contentString = await result.Content.ReadAsStringAsync();
             Assert.Contains("Hello from ProductsController", contentString);
-        }
-
-        private class TestApplicationEnvironment : IApplicationEnvironment
-        {
-            private static readonly Assembly TestAssembly = typeof(NamespaceRouting.Startup).GetTypeInfo().Assembly;
-
-            public string ApplicationBasePath { get; } = TestAssembly.Location;
-
-            public string ApplicationName { get; set; } = TestAssembly.GetName().Name;
-
-            public string ApplicationVersion { get; set; }
-
-            public FrameworkName RuntimeFramework { get; set; }
         }
     }
 }
