0ac2fdc46721340b7f50bccc2070bde285b135a3
Pranav K <prkrishn@hotmail.com>
2016-03-25 14:22:53 -0700

    Reacting to Mvc changes

--- samples/MusicStore.Spa/Infrastructure/SortExpression.cs
+++ samples/MusicStore.Spa/Infrastructure/SortExpression.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
 
 namespace MusicStore.Infrastructure
 {
