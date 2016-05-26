9bdbc597195ed828cea6bc0e38e5efb170f37bd5
Hao Kung <haok@microsoft.com>
2016-05-26 16:44:12 -0700

    React to Configuration API changes

--- samples/Mvc.FileUpload/Startup.cs
+++ samples/Mvc.FileUpload/Startup.cs
@@ -13,11 +13,7 @@ namespace Mvc.FileUpload
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile(source =>
-                {
-                    source.Path = "appsettings.json";
-                    source.ReloadOnChange = true;
-                })
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
