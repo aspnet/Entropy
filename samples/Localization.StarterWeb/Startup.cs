7ed8392dfdae67c140e14d321bc2b1a7b9a6993f
Kiran Challa <kichalla@microsoft.com>
2016-08-05 10:34:38 -0700

    Revert "Temporary fix for build break"
    This reverts commit 649767d67f3a3666e4bb1eb36baa9133a16aa51d.

--- samples/Localization.StarterWeb/Startup.cs
+++ samples/Localization.StarterWeb/Startup.cs
@@ -148,6 +148,7 @@ namespace Localization.StarterWeb
             var config = new ConfigurationBuilder().AddCommandLine(args).Build();
 
             var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseKestrel()
                 .UseConfiguration(config)
                 .UseIISIntegration()
