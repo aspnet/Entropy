b7a15c6e49b2140c40e40f3f6c15a418ce4c8cb2
Pranav K <prkrishn@hotmail.com>
2016-11-11 09:48:28 -0800

    Fix build warnings

--- samples/Localization.StarterWeb/Startup.cs
+++ samples/Localization.StarterWeb/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.IO;
 
 namespace Localization.StarterWeb
 {
