1d9c57fd1fe335ec6bd154db2b5b7b5480409fb8
Pranav K <prkrishn@hotmail.com>
2016-12-28 16:58:31 -0800

    Fix more tests broken due to Localization

--- test/FunctionalTests/MvcTests/LocalizationSampleTest.cs
+++ test/FunctionalTests/MvcTests/LocalizationSampleTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#if NET451
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -142,3 +143,4 @@ mypartial
         }
     }
 }
+#endif
