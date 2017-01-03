b055afa3904185b05ff3298e15eb4dab23f026dd
BrennanConroy <brecon@microsoft.com>
2017-01-03 10:41:17 -0800

    Temporarily skip more localizations tests
    Due to #186

--- test/FunctionalTests/LocalizationTests/LocalizationWebTests.cs
+++ test/FunctionalTests/LocalizationTests/LocalizationWebTests.cs
@@ -30,8 +30,8 @@ namespace EntropyTests.LocalizationTests
         [ConditionalTheory]
         [OSSkipCondition(OperatingSystems.Windows)]
         [InlineData(ServerType.Kestrel, RuntimeFlavor.Clr, RuntimeArchitecture.x64, "http://localhost:6204", Skip = "Disabled due to https://github.com/dotnet/corefx/issues/9012")]
-        [InlineData(ServerType.Kestrel, RuntimeFlavor.CoreClr, RuntimeArchitecture.x64, "http://localhost:6205")]
-        [InlineData(ServerType.Nginx, RuntimeFlavor.CoreClr, RuntimeArchitecture.x64, "http://localhost:6206")]
+        [InlineData(ServerType.Kestrel, RuntimeFlavor.CoreClr, RuntimeArchitecture.x64, "http://localhost:6205", Skip = "https://github.com/aspnet/Entropy/issues/186")]
+        [InlineData(ServerType.Nginx, RuntimeFlavor.CoreClr, RuntimeArchitecture.x64, "http://localhost:6206", Skip = "https://github.com/aspnet/Entropy/issues/186")]
         public async Task RunSite_NonWindowsOnly(ServerType server, RuntimeFlavor runtimeFlavor, RuntimeArchitecture architecture, string applicationBaseUrl)
         {
             await RunSite(server, runtimeFlavor, architecture, applicationBaseUrl);
