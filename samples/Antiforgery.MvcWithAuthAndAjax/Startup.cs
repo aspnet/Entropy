024d9b65c0bede75b8b112574f8dd6cd7220a816
Hao Kung <haok@microsoft.com>
2017-04-19 19:46:04 -0700

    Fix ajax startup

--- samples/Antiforgery.MvcWithAuthAndAjax/Startup.cs
+++ samples/Antiforgery.MvcWithAuthAndAjax/Startup.cs
@@ -24,7 +24,7 @@ namespace Antiforgery.MvcWithAuthAndAjax
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
-            services.ConfigureApplicationCookie(options => options.Cookies.ApplicationCookie.AccessDeniedPath = "/Home/AccessDenied");
+            services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Home/AccessDenied");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
