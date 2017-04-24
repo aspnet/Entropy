72be2042ee2bd53a8f07a1526ecca0e3add456ad
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2017-04-24 16:05:19 -0700

    remove integration test logging (#228)
    now that logging.testing has it

--- test/Entropy.FunctionalTests/E2ETestBase.cs
+++ test/Entropy.FunctionalTests/E2ETestBase.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -7,9 +7,9 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Server.IntegrationTesting;
-using Microsoft.AspNetCore.Server.IntegrationTesting.xunit;
 using Microsoft.AspNetCore.Testing.xunit;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Testing;
 using Xunit;
 using Xunit.Abstractions;
 
