7f3238253b2085444ae521ca77fbc24378a39217
Mike Harder <mharder@microsoft.com>
2017-05-04 15:38:23 -0700

    netcoreapp2.0 (#234)
    - Remove Owin samples which cannnot target netcoreapp2.0
      - Packages `Microsoft.Owin` and `Nowin` are not compatible with `netcoreapp2.0`
    
    - Convert `Mvc.LocalizationSample.Web` from `net46` to `netcoreapp2.0`
      - Was converted to `net46` to workaround a temporary issue in `netcoreapp2.0` related to satellite assemblies

--- test/Entropy.FunctionalTests/MvcTests/LocalizationSampleTest.cs
+++ test/Entropy.FunctionalTests/MvcTests/LocalizationSampleTest.cs
@@ -1,7 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-#if NET46
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -143,7 +142,3 @@ mypartial
         }
     }
 }
-#elif NETCOREAPP2_0
-#else
-#error Target framework needs to be updated
-#endif
